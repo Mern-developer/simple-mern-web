3b8ac02c46de52fc2daf8cf78a0b528f00def3b5
Farhan Abbasi <mernstackdeveloper@yahoo.com>
2022-08-05 20:45:45 +0500

    Simple Mern first commiet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from '../src/Components/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+<StoreProvider>
     <App />
+
+</StoreProvider>
+
   </React.StrictMode>
 );
 
